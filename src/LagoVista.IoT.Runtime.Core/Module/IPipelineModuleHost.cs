3d75ef670b5b66ac13af346e86e2295f2e472e7c
Kevin D. Wolf <kevinw@software-logistics.com>
2017-04-27 10:20:16 -0400

    update dependencies

--- src/LagoVista.IoT.Runtime.Core/Module/IPipelineModuleHost.cs
+++ src/LagoVista.IoT.Runtime.Core/Module/IPipelineModuleHost.cs
@@ -2,19 +2,7 @@
 using System.Threading.Tasks;
 
 namespace LagoVista.IoT.Runtime.Core.Module
-{
-    public enum DeploymentHostStates
-    {
-        New,
-        Initializing,
-        Running,
-        Paused,
-        Stopping,
-        Stopped,
-        Degraded,
-        FatalError,
-    }
-
+{    
     public interface IDeploymentHost : IDisposable
     {
         DeploymentHostStates Status { get; }

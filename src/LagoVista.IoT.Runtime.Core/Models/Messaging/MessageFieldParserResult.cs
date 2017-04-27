3af60d2ec25da0f2058f40e3b4d3e57ba2ac5b22
Kevin D. Wolf <kevinw@software-logistics.com>
2017-04-27 10:44:13 -0400

    add interface for parser manager

---
+++ src/LagoVista.IoT.Runtime.Core/Models/Messaging/MessageFieldParserResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Runtime.Core.Models.Messaging
+{
+    public struct MessageFieldParserResult
+    {
+        public bool Success { get; set; }
+
+        public string Result { get; set; }
+
+        public static MessageFieldParserResult FromSuccess(String result)
+        {
+            return new MessageFieldParserResult()
+            {
+                Success = true,
+                Result = result
+            };
+        }
+
+        public static MessageFieldParserResult FromFailure()
+        {
+            return new MessageFieldParserResult()
+            {
+                Success = false,
+            };
+        }
+    }
+}
---
+++ src/LagoVista.IoT.Runtime.Core/Module/IParserManager.cs
@@ -0,0 +1,26 @@
+﻿using LagoVista.Core.PlatformSupport;
+using LagoVista.IoT.Pipeline.Admin.Models;
+using LagoVista.IoT.Runtime.Core.Models.Messaging;
+using LagoVista.IoT.Runtime.Core.Models.PEM;
+
+namespace LagoVista.IoT.Runtime.Core.Module
+{
+    public enum ParserTypes
+    {
+        Text,
+        Binary,
+        TextOrBinary
+    }
+
+    public interface IMessageFieldParser
+    {
+        MessageFieldParserResult Parse(PipelineExectionMessage msg);
+
+        ParserTypes ParserType { get; }
+    }
+    public interface IParserManager
+    {
+
+        IMessageFieldParser GetFieldMessageParser(IMessageFieldParserConfiguration parserConfig, ILogger logger);
+    }
+}

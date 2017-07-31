17e429800d085d231aea981dd1b7c855bb866536
Kevin D. Wolf <kevinw@software-logistics.com>
2017-07-31 18:52:31 -0400

    integration/deploy containers working

---
+++ src/LagoVista.IoT.Runtime.Core/Models/HostVersion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Runtime.Core.Models
+{
+    public class HostVersion
+    {
+        public string Version { get; set; }
+        public string BuildDate { get; set; }
+    }
+}

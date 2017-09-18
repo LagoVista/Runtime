64e5c0099ef72dde7e8c5e6ef28dcaf9bcb2bc05
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-09-18 08:19:26 -0400

    create interface for geocode parser

---
+++ src/LagoVista.IoT.Runtime.Core/Interfaces/IGeoParser.cs
@@ -0,0 +1,15 @@
+﻿using LagoVista.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Runtime.Core
+{
+    /// <summary>
+    /// To be used to take lat/lon obects and attempt to turn them into a string that is a lat and a lon
+    /// </summary>
+    public interface IGeoParser
+    {
+        InvokeResult<String> Parse(object lat, object lon); 
+    }
+}

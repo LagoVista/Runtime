7eaf4d7c58927ccc2cc172531a3ae0cddf5d5467
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-10-08 17:55:28 -0500

    Scripting is getting very close

--- src/LagoVista.IoT.Runtime.Core/Models/PEM/MessageValue.cs
+++ src/LagoVista.IoT.Runtime.Core/Models/PEM/MessageValue.cs
@@ -29,7 +29,17 @@ namespace LagoVista.IoT.Runtime.Core.Models.PEM
         public string Value
         {
             get { return _value; }
-            set { _value = value; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    _value = null;
+                }
+                else
+                {
+                    _value = value;
+                }
+            }
         }
         public EntityHeader<UnitSet> UnitSet { get; set; }
         public EntityHeader<StateSet> StateSet { get; set; }
@@ -40,7 +50,7 @@ namespace LagoVista.IoT.Runtime.Core.Models.PEM
             return StateSet.Value.States.Where(state => state.Key == Value).FirstOrDefault();
         }
 
-        private double GetDouble()
+        public double GetDouble()
         {
             return double.Parse(Value);
         }
@@ -107,34 +117,34 @@ namespace LagoVista.IoT.Runtime.Core.Models.PEM
                     if (!Value.SuccessfulJSONDate()) result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidDateTime.ToErrorMessage($"Key={Key},Value={Value}"));
                     break;
                 case ParameterTypes.Decimal:
-                    if (!decimal.TryParse(Value, out decimal decValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.ValueDecimalINotDecimal.ToErrorMessage($"Key={Key},Value={Value}"));
+                    if (!decimal.TryParse(Value, out decimal decValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.InvlidDecimal.ToErrorMessage($"Key={Key},Value={Value}"));
                     break;
                 case ParameterTypes.GeoLocation:
                     var regEx = new Regex(@"^(?'lat'-?\d{1,2}\.\d{6}),(?'lon'-?\d{1,3}\.\d{6})$");
                     var match = regEx.Match(Value);
                     if (!match.Success)
                     {
-                        result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidGeoLocationFormation.ToErrorMessage($"Key={Key},Value={Value}"));
+                        result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidGeoLocation.ToErrorMessage($"Key={Key},Value={Value}"));
                     }
                     else
                     {
                         if (double.TryParse(match.Groups["lat"].Value, out double lat) && double.TryParse(match.Groups["lon"].Value, out double lon))
                         {
-                            if (lat > 90 || lat < -90) result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidGeoLocationFormation.ToErrorMessage($"Key={Key},Latitude={lat}"));
-                            if (lon > 180 || lon < -180) result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidGeoLocationFormation.ToErrorMessage($"Key={Key},Longitude={lon}"));
+                            if (lat > 90 || lat < -90) result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidLatitude.ToErrorMessage($"Key={Key},Latitude={lat}"));
+                            if (lon > 180 || lon < -180) result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidLongitude.ToErrorMessage($"Key={Key},Longitude={lon}"));
                         }
                         else
                         {
-                            result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidGeoLocationFormation.ToErrorMessage($"Key={Key},Value={Value}"));
+                            result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidGeoLocation.ToErrorMessage($"Key={Key},Value={Value}"));
                         }
                     }
                     break;
                 case ParameterTypes.Integer:
-                    if (!int.TryParse(Value, out int intValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.ValueOnUnitSetnotDouble.ToErrorMessage($"Key={Key},Value={Value}"));
+                    if (!int.TryParse(Value, out int intValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.InvalidInteger.ToErrorMessage($"Key={Key},Value={Value}"));
                     break;
                 case ParameterTypes.State:
-                    if (EntityHeader.IsNullOrEmpty(UnitSet)) result.Errors.Add(Resources.ErrorCodes.Messaging.StateSetSpecifiedButNotProvided.ToErrorMessage($"Key={Key}"));
-                    if (!StateSet.Value.States.Where(state => state.Key == Value).Any())
+                    if (EntityHeader.IsNullOrEmpty(StateSet)) result.Errors.Add(Resources.ErrorCodes.Messaging.StateSetSpecifiedButNotProvided.ToErrorMessage($"Key={Key}"));
+                    if (result.Successful && !StateSet.Value.States.Where(state => state.Key == Value).Any())
                     {
                         var allowable = string.Empty;
                         foreach (var state in StateSet.Value.States)
@@ -147,11 +157,11 @@ namespace LagoVista.IoT.Runtime.Core.Models.PEM
                 case ParameterTypes.String: /* No Op, if we have a string it's valid */
                     break;
                 case ParameterTypes.TrueFalse:
-                    if (!bool.TryParse(Value, out bool boolValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.ValueOnUnitSetnotDouble.ToErrorMessage($"Key={Key},Value={Value}"));
+                    if (!bool.TryParse(Value, out bool boolValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.ValueOnUnitSetNotDouble.ToErrorMessage($"Key={Key},Value={Value}"));
                     break;
                 case ParameterTypes.ValueWithUnit:
                     if (EntityHeader.IsNullOrEmpty(UnitSet)) result.Errors.Add(Resources.ErrorCodes.Messaging.UnitTypeSpecifiedButnotProvided.ToErrorMessage($"Key={Key}"));
-                    if (!double.TryParse(Value, out double dblValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.ValueOnUnitSetnotDouble.ToErrorMessage($"Key={Key},Value={Value}"));
+                    if (!double.TryParse(Value, out double dblValue)) result.Errors.Add(Resources.ErrorCodes.Messaging.ValueOnUnitSetNotDouble.ToErrorMessage($"Key={Key},Value={Value}"));
                     break;
             }
 
--- tests/LagoVista.IoT.Core.Runtime.Tests/Messaging/MessageValueTests.cs
+++ tests/LagoVista.IoT.Core.Runtime.Tests/Messaging/MessageValueTests.cs
@@ -12,11 +12,15 @@ namespace LagoVista.IoT.Core.Runtime.Tests.Messaging
         private void AssertInValid(ValidationResult result, params string[] errorCodes)
         {
             Assert.IsFalse(result.Successful);
+            foreach (var err in result.Errors)
+            {
+                Console.WriteLine(err.Message + "  " + err.Details);
+            }
+
             foreach (var err in errorCodes)
             {
                 var validationError = result.Errors.Where(er => er.ErrorCode == err).FirstOrDefault();
                 Assert.IsNotNull(validationError);
-                Console.WriteLine(validationError.Message + "  " + validationError.Details);
             }
         }
 
@@ -53,6 +57,47 @@ namespace LagoVista.IoT.Core.Runtime.Tests.Messaging
         }
 
         [TestMethod]
+        public void MessageValue_HasValue()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
+            {
+                Key = "one",
+                Value = "1234"
+            };
+
+            AssertValid(msgValue.Validate());
+            Assert.IsTrue(msgValue.HasValue);
+        }
+
+
+        [TestMethod]
+        public void MessageValue_DoesNotHavValue_Null()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
+            {
+                Key = "one",
+                Value = null
+            };
+
+            AssertValid(msgValue.Validate());
+            Assert.IsFalse(msgValue.HasValue);
+        }
+
+        [TestMethod]
+        public void MessageValue_DoesNotHavValue_EmptyString()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
+            {
+                Key = "one",
+                Value = String.Empty
+            };
+
+            AssertValid(msgValue.Validate());
+            Assert.IsFalse(msgValue.HasValue);
+            Assert.IsNull(msgValue.Value);
+        }
+
+        [TestMethod]
         public void MessageValue_Int_Valid()
         {
             var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
@@ -63,5 +108,322 @@ namespace LagoVista.IoT.Core.Runtime.Tests.Messaging
 
             AssertValid(msgValue.Validate());
         }
+
+        [TestMethod]
+        public void MessageValue_Int_Parsed()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
+            {
+                Key = "one",
+                Value = "1234"
+            };
+
+           Assert.AreEqual(1234, msgValue.GetIntValue());
+        }
+
+        [TestMethod]
+        public void MessageValue_Int_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
+            {
+                Key = "one",
+                Value = "1234AB"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidInteger.Code);
+        }
+
+
+        [TestMethod]
+        public void MessageValue_IntWithDecimalPoints_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Integer)
+            {
+                Key = "one",
+                Value = "1234.45"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidInteger.Code);
+        }
+
+
+        [TestMethod]
+        public void MessageValue_Decimal_Valid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Decimal)
+            {
+                Key = "one",
+                Value = "1234.54"
+            };
+
+            AssertValid(msgValue.Validate());
+        }
+
+        [TestMethod]
+        public void MessageValue_Decimal_Parsed()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Decimal)
+            {
+                Key = "one",
+                Value = "1234.54"
+            };
+
+            Assert.AreEqual(1234.54, msgValue.GetDouble());
+        }
+
+        [TestMethod]
+        public void MessageValue_Decimal_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.Decimal)
+            {
+                Key = "one",
+                Value = "1234AB"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvlidDecimal.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_DateTime_Valid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.DateTime)
+            {
+                Key = "one",
+                Value = "2017-10-08T03:30:00.000Z"
+            };
+
+            AssertValid(msgValue.Validate());
+        }
+
+
+        [TestMethod]
+        public void MessageValue_DateTime_Parsed()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.DateTime)
+            {
+                Key = "one",
+                Value = "2017-10-08T03:30:00.000Z"
+            };
+            var date = msgValue.GetUniversalDateTime();
+
+            Assert.AreEqual(2017, date.Year);
+            Assert.AreEqual(10, date.Month);
+            Assert.AreEqual(8, date.Day);
+            Assert.AreEqual(3, date.Hour);
+            Assert.AreEqual(30, date.Minute);
+        }
+
+        [TestMethod]
+        public void MessageValue_DateTime_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.DateTime)
+            {
+                Key = "one",
+                Value = "2017-10-08T03:30.000Z"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidDateTime.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_DateTime_OutOfRange_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.DateTime)
+            {
+                Key = "one",
+                Value = "2017-10-08T03:30:90.000Z"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidDateTime.Code);
+        }
+
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_Valid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "22.234512,-122.423413"
+            };
+
+            AssertValid(msgValue.Validate());
+        }
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_Parsed()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "22.234512,-122.423413"
+            };
+
+            AssertValid(msgValue.Validate());
+            Assert.AreEqual(22.234512, msgValue.GetGeoLocation().Latitude);
+            Assert.AreEqual(-122.423413, msgValue.GetGeoLocation().Longitude);
+        }
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "22.234512,-122.4234dd13"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidGeoLocation.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_LatOutOfRanagePositive_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "92.234512,-122.423413"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidLatitude.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_LatOutOfRanageNegative_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "-92.234512,-122.423213"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidLatitude.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_LonOutOfRanagePositive_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "22.234512,-180.423413"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidLongitude.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_GeoLocation_LonOutOfRanageNegative_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.GeoLocation)
+            {
+                Key = "one",
+                Value = "-22.234512,-180.423213"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidLongitude.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_StateSet_Valid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.State)
+            {
+                Key = "one",
+                Value = "state1",
+                StateSet = new LagoVista.Core.Models.EntityHeader<DeviceAdmin.Models.StateSet>()
+                {
+                    Id="abc123",
+                    Value=new DeviceAdmin.Models.StateSet()
+                    {
+                        States = new System.Collections.Generic.List<DeviceAdmin.Models.State>()
+                        {
+                            new DeviceAdmin.Models.State() { Key="state1"},
+                            new DeviceAdmin.Models.State() { Key="state2"},
+                        }
+                    }
+                }
+            };
+
+            AssertValid(msgValue.Validate());
+        }
+
+
+        [TestMethod]
+        public void MessageValue_StateSet_MissingStateSet_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.State)
+            {
+                Key = "one",
+                Value = "state1"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.StateSetSpecifiedButNotProvided.Code);
+        }
+
+        [TestMethod]
+        public void MessageValue_StateSet_InvalidState_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.State)
+            {
+                Key = "one",
+                Value = "doesntexist",
+                StateSet = new LagoVista.Core.Models.EntityHeader<DeviceAdmin.Models.StateSet>()
+                {
+                    Id = "abc123",
+                    Value = new DeviceAdmin.Models.StateSet()
+                    {
+                        States = new System.Collections.Generic.List<DeviceAdmin.Models.State>()
+                        {
+                            new DeviceAdmin.Models.State() { Key="state1"},
+                            new DeviceAdmin.Models.State() { Key="state2"},
+                        }
+                    }
+                }
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.InvalidStateValue.Code);
+        }
+
+
+        [TestMethod]
+        public void MessageValue_UnitSet_Valid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.ValueWithUnit)
+            {
+                Key = "one",
+                Value = "32.5",
+                UnitSet = new LagoVista.Core.Models.EntityHeader<DeviceAdmin.Models.UnitSet>() { Id="123", Text="abc", Value = new DeviceAdmin.Models.UnitSet() { Id="123", Key="abc"} }
+            };
+
+            AssertValid(msgValue.Validate());
+        }
+
+        [TestMethod]
+        public void MessageValue_UnitSet_MissingUnitSet_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.ValueWithUnit)
+            {
+                Key = "one",
+                Value = "state1"
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.UnitTypeSpecifiedButnotProvided.Code);
+        }
+
+
+        [TestMethod]
+        public void MessageValue_UnitSet_InvalidValue_InValid()
+        {
+            var msgValue = new MessageValue(DeviceAdmin.Models.ParameterTypes.ValueWithUnit)
+            {
+                Key = "one",
+                Value = "32.f",
+                UnitSet = new LagoVista.Core.Models.EntityHeader<DeviceAdmin.Models.UnitSet>() { Id = "123", Text = "abc", Value = new DeviceAdmin.Models.UnitSet() { Id = "123", Key = "abc" } }
+            };
+
+            AssertInValid(msgValue.Validate(), LagoVista.IoT.Runtime.Core.Resources.ErrorCodes.Messaging.ValueOnUnitSetNotDouble.Code);
+        }
+
     }
 }

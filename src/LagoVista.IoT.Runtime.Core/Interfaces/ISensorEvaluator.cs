db81910169fd93bdad6c6a7f6996cacd73247d8c
Kevin D. Wolf <kevinw@software-logistics.com>
2021-09-06 06:27:42 -0400

    interface for sensor evaluator

---
+++ src/LagoVista.IoT.Runtime.Core/Interfaces/ISensorEvaluator.cs
@@ -0,0 +1,10 @@
+﻿using LagoVista.IoT.DeviceManagement.Core.Models;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.Runtime.Core.Interfaces
+{
+    public interface ISensorEvaluator
+    {
+        Task EvaluateAsync(Device device);
+    }
+}

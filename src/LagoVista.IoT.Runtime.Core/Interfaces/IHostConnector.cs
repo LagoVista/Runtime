22d6e04e1197512eb643bab11cb1e331faf579cf
Kelvin McDaniel <kelvin.mcdaniel@live.com>
2022-12-13 13:25:28 -0500

    change interface to match implementation provided by Kevin.

---
+++ src/LagoVista.IoT.Runtime.Core/Interfaces/IHostConnector.cs
@@ -0,0 +1,14 @@
+﻿using LagoVista.Core.Validation;
+using LagoVista.IoT.Deployment.Admin.Models;
+using LagoVista.IoT.Deployment.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.Runtime.Core.Interfaces
+{
+	public interface IHostConnector
+	{
+		Task<InvokeResult<DeploymentHost>> GetHostAsync();
+		Task<InvokeResult<List<SharedInstanceSummary>>> GetInstanceAsync();
+	}
+}

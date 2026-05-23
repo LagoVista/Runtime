9233db1a0873b6a598d9e8a85874b31ba67c8064
Kevin D. Wolf <kevinw@software-logistics.com>
2026-05-23 17:40:11 -0400

    compute metrics

--- src/LagoVista.IoT.Runtime.Core/Interfaces/IComputeServicesMonitor.cs
+++ src/LagoVista.IoT.Runtime.Core/Interfaces/IComputeServicesMonitor.cs
@@ -2,6 +2,7 @@
 // ContentHash: 11d2fcc37be8c8c09856fa59b453c519d2e1d7a9ab731d0a9fcce88e18f3d1a4
 // IndexVersion: 2
 // --- END CODE INDEX META ---
+using LagoVista.Core.Models;
 using LagoVista.IoT.Runtime.Core.Models;
 using System;
 using System.Collections.Generic;
--- src/LagoVista.IoT.Runtime.Core/Storage/IComputeResourceMetricsRepo.cs
+++ src/LagoVista.IoT.Runtime.Core/Storage/IComputeResourceMetricsRepo.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using LagoVista.Core.Models;
 
 namespace LagoVista.IoT.Runtime.Core.Storage
 {
